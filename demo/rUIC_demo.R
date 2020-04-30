2c681dc2f006e46a824e1e49d110984da2c9c9fd
Masayuki Ushio <ong8181@gmail.com>
2020-04-30 11:39:45 +0900

    Add files via upload

--- demo/rUIC_demo.R
+++ demo/rUIC_demo.R
@@ -28,7 +28,7 @@ block <- data.frame(t = 1:tl, x = x, y = y)
 block_melted <- block %>% pivot_longer(col = c(x,y), names_to = "variable", values_to = "value")
 ts <- ggplot(block_melted, aes(x = t, y = value, color = variable)) + geom_line() +
   scale_color_manual(values = c("red3", "royalblue")) + 
-  ggtitle("Model time series") + ylab("Value") + xlab("Time") + xlim(100,200)
+  ylab("Value") + xlab("Time") + xlim(100,200)
 
 # No.1: Determine the optimal embedding dimension using simplex projection
 ## Univariate UIC-version simplex projection
@@ -58,51 +58,51 @@ g1 <- ggplot(simp_x, aes(x = E, y = rmse)) + geom_line() +
   geom_point(aes(color = pval < 0.05), size = 2) +
   scale_color_manual(values = c("black", "red3")) +
   ylab("RMSE") + theme(legend.position = "none") +
-  ggtitle(expression("{"~X[t]~","~X[t-1]~", ...}"))
+  ggtitle(expression("{"~x[t]~","~y[t-1]~", ... }"))
 g2 <- ggplot(simp_y, aes(x = E, y = rmse)) + geom_line() +
   geom_point(aes(color = pval < 0.05), size = 2) +
   scale_color_manual(values = c("black", "red3")) +
   ylab("RMSE") + theme(legend.position = "none") +
-  ggtitle(expression("{"~Y[t]~","~Y[t-1]~", ...}"))
+  ggtitle(expression("{"~y[t]~","~y[t-1]~", ... }"))
 g3 <- ggplot(simp_xy, aes(x = E, y = rmse)) + geom_line() +
   geom_point(aes(color = pval < 0.05), size = 2) +
   scale_color_manual(values = c("black", "red3")) +
   ylab("RMSE") + theme(legend.position = "none") +
-  ggtitle(expression("{"~X[t]~","~X[t-1]~", ..."~Y[t]~"}"))
+  ggtitle(expression("{"~x[t]~","~x[t-1]~", ..."~y[t]"}"))
 g4 <- ggplot(simp_yx, aes(x = E, y = rmse)) + geom_line() +
   geom_point(aes(color = pval < 0.05), size = 2) +
   scale_color_manual(values = c("black", "red3")) +
   ylab("RMSE") + theme(legend.position = "none") +
-  ggtitle(expression("{"~Y[t]~","~Y[t-1]~", ..."~X[t]~"}"))
+  ggtitle(expression("{"~y[t]~","~y[t-1]~", ..."~x[t]"}"))
 
 # Visualize cross-map
 g5 <- ggplot(xmap_xy$model_output, aes(x = data, y = pred)) +
   geom_abline(intercept = 0, slope = 1, linetype = 2, color = "red3") +
   xlim(0.19, 0.9) + ylim(0.19, 0.9) +
   geom_point(alpha = 0.7) + xlab("Observed") + ylab("Predicted") +
-  ggtitle(expression("X cross-map Y (Y cause X?)"))
+  ggtitle(expression("x cross-map y (y cause x?)"))
 g6 <- ggplot(xmap_yx$model_output, aes(x = data, y = pred)) +
   geom_abline(intercept = 0, slope = 1, linetype = 2, color = "red3") +
   xlim(0.15, 1) + ylim(0.15, 1) + 
   geom_point(alpha = 0.7) + xlab("Observed") + ylab("Predicted") +
-  ggtitle(expression("Y cross-map X (X cause Y?)"))
+  ggtitle(expression("y cross-map x (x cause y?)"))
 
 # Visualize UIC
 g7 <- ggplot(uic_xy, aes(x = tp, y = te)) + geom_line() +
   geom_point(aes(color = pval < 0.05), size = 2) + xlab("tp") + ylab("UIC") + 
   scale_color_manual(values = c("black", "red3")) +
   scale_x_continuous(breaks = -4:5) +
-  ylim(-0.01, 0.4) + ggtitle(expression("UIC X" %<-% "Y (Y cause X?)")) + theme(legend.position = "none")
+  ylim(-0.01, 0.4) + ggtitle(expression("UIC (y cause x?)")) + theme(legend.position = "none")
 g8 <- ggplot(uic_yx, aes(x = tp, y = te)) + geom_vline(xintercept = -1, size = 3, alpha = 0.2) +
   geom_line() + scale_x_continuous(breaks = -4:5) +
   geom_point(aes(color = pval < 0.05), size = 2) + xlab("tp") + ylab("UIC") +
   scale_color_manual(values = c("black", "red3")) +
-  ylim(-0.01, 0.4) + ggtitle(expression("UIC Y" %<-% "X (X cause Y?)")) + theme(legend.position = "none")
+  ylim(-0.01, 0.4) + ggtitle(expression("UIC (x cause y?)")) + theme(legend.position = "none")
   
 # Save figures
 ggsave("demo_figures/time_series.png",
        plot = ts,
-       width = 8, height = 4)
+       width = 8, height = 3)
 
 ggsave("demo_figures/simplex_rmse.png",
        plot = plot_grid(g1, g2, g3, g4, nrow = 2, align = "hv"),
@@ -114,4 +114,4 @@ ggsave("demo_figures/xmap.png",
 
 ggsave("demo_figures/uic.png",
        plot_grid(g7, g8, nrow = 1, align = "hv"),
-       width = 8, height = 4)
+       width = 8, height = 3.5)
